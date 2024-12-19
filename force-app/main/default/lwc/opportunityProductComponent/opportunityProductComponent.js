6ac8167a8d66994b2a6db9172f849fdacd595cf9
Clemz68 <clement.glodas@gmail.com>
2024-12-19 13:35:47 +0100

    :wrench: chore (init): projet init structure

---
+++ force-app/main/default/lwc/opportunityProductComponent/opportunityProductComponent.js
@@ -0,0 +1,121 @@
+import { LightningElement, wire, track, api } from 'lwc';
+import getOpportunityProductList from '@salesforce/apex/OpportunityProductController.getOpportunityProductList';
+import getSystemProfile from '@salesforce/apex/SystemProfile.getSystemProfile';
+import deleteOppLineItem from '@salesforce/apex/OpportunityProductController.deleteOppLineItem';
+
+
+const columns = [
+    {label: 'Item Id', fieldName : 'Id', type: 'Id'},
+    {label: 'Nom du produit', fieldName: 'productName', type: 'text' },
+    {label: 'Quantité', fieldName: 'Quantity', type: 'number', 
+    cellAttributes: { alignment:'left', class: {fieldName: 'quantityClass'} }},// fieldname rend la chose dynamique car je pointe vers un proprité de donnée qui peux changer dynamiquement
+    {label: 'Prix unitaire', fieldName: 'UnitPrice', type: 'currency' },
+    {label: 'Prix total', fieldName: 'TotalPrice', type: 'currency' },
+    {label: 'Quantité en Stock', fieldName: 'QuantityInStock', type: 'number'},
+    {label: 'Voir produit', fieldName: 'VoirProduit', type: 'button', initialWidth: 175, 
+    typeAttributes: { iconName: 'utility:preview', label: 'Voir Produit', variant:'brand', name: 'voir_produit', title: 'Cliquez pour voir le produit', },
+    cellAttributes: { alignment: 'center',}},
+    {label: 'Supprimer', type: 'button-icon', typeAttributes: { iconName: 'utility:delete', name: 'supprimer', title: 'Clickez pour supprimer'}},
+];
+
+export default class OpportunityProductComponent extends LightningElement {
+    @api recordId;
+    @track opportunityLineItem = [];
+    columns = columns;
+    noproduct = true;
+    showMessage = false;
+   
+    // Pas besoin de mettre let ou const car ce sont implicitement des paramètres de la classe
+    
+    @wire(getOpportunityProductList, { opportunityId: '$recordId' })
+    wiredgetOpportunityProductList({ data, error }) {
+        
+        if (data) {
+            let quantityClass = '';
+            this.columns = this.specificitySalesProfile(columns); // Appel à fetchUserProfile pour savoir si on affiche la column "voir produit"
+            this.opportunityLineItem = data.map((record) => {// Aplatir les données pour faciliter l'accès aux champs imbriqués (en locurence product name et quantity in stock)
+            quantityClass = this.quantityAlert (record); // Appel à quantityAlert pour déterminer la quantityClass et mettre a jour showmessage si besoin
+
+            return {... record, 
+            quantityClass: quantityClass, //Appliquer le style css récupéré via la méthode à la colonne Quantity 
+            productName: record.Product2.Name, 
+            QuantityInStock: record.Product2.QuantityInStock__c,  
+        }}),
+
+
+       // Vérifier si la opplineitem a des valeurs si il y a des valeurs noproduct = true sinon false 
+            console.log(this.opportunityLineItem);
+            console.log ()
+            this.noproduct = this.opportunityLineItem.length === 0;
+            // si le tableau est vide, this.noproduct = true autrement = false
+
+        } else if (error) {
+            console.error('Erreur lors de la récupération des données :', error);
+            this.opportunityLineItem = [];
+            this.noproduct = true;
+        }
+    }
+
+
+    quantityAlert(record) {
+    
+        if (record.Quantity > record.Product2.QuantityInStock__c) {
+            this.showMessage = true
+            return 'slds-text-color_error slds-theme_shade slds-theme_alert-texture slds-text-bold'
+        } else {
+            return 'slds-text-color_success slds-text-bold'
+        }
+    } // La méthode prend en paramètre l'enregistrement itéré, modifie showMessage selon la logique, et renvoie le style Css correspondant  
+
+    specificitySalesProfile(){
+        
+        let userProfileName = getSystemProfile ()
+    
+        .then((userProfileName) => {
+            console.log(`thenCatchApproach result =>`+ userProfileName);
+            
+            if (userProfileName === 'Commercial') {
+               this.columns = [...columns].filter(columns => columns.fieldName != 'VoirProduit');
+            }// Si le profil est system administrator modifier le tableau columns en enlevant celui la colonne voir produit
+        })
+        .catch((error) => {
+          console.log(`thenCatchApproach error => `);
+        });
+
+        return columns;
+    } // La méthode prend pas de paramètre et modifie le tableau en enlevant la colonne VoirProduit si le profil (obtenu en appelant une classe apex) est commercial
+
+    handleRowAction(event) {
+        const columnName = event.detail.action.name;
+        const itemToDelete = event.detail.row.Id;
+        const itemToView = event.detail.row.Product2.Id;
+        
+        console.log('Voici le id'+ itemToDelete)
+
+        switch (columnName){
+        
+        case 'supprimer': 
+
+            deleteOppLineItem (itemToDelete)
+                .then(() => {
+                    console.log('succès de la suppr');
+                    // rafraichir les données
+                })
+                .catch((error) => {
+                    console.log('erreur de la suppr');
+                });// Appel de la méthode apex pour supprimer l'enregistrement correspondant à la ligne
+                // Penser à permettre de supprimer la ligne également
+                // La méthode retourne une Promesse (promise) car elle effectue une requete serveur via apex (et elle n'est pas wired)
+            break
+            
+        case 'voir_produit':
+
+            window.open("/lightning/r/Product2/"+itemToView+"/view",'_blank');
+            // Ouvre l'url mentionné dans un nouvel onglet
+            break
+
+        default: 
+        }
+    }  // La méthode récupère l'évènement rowaction en paramètre et en fonction du bouton cliqué exécute du code
+     
+}
